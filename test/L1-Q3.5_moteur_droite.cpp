77c6564012e8048073971bb38351d116452271ff
Eliot GGT <150823823+PhantmGC@users.noreply.github.com>
2026-02-04 10:29:25 +0100

    Modify motor control timing and speed settings
    Updated the timing and motor control logic in the loop function.

--- test/L1-Q3.5_moteur_droite.cpp
+++ test/L1-Q3.5_moteur_droite.cpp
@@ -13,10 +13,11 @@ Encoder knobRight(19, 27);
 
 // Paramètres de mesure
 unsigned long previousMicros = 0;
-const unsigned long TE_US = 1000;    // TE
+const unsigned long TE_US = 2000;    // TE
 const float N_IMP = 1204.0;         // Nombre d'incréments par tour
 
 long oldRight = 0;
+unsigned long debutEchelon = 0;
 int cas = 0;
 
 void initMoteurs() {
@@ -39,36 +40,55 @@ void setup() {
   sei();
   digitalWrite(43, 1);  
 
-  Serial.println("Vitesse_D(tr/min)");
+  Serial.println("Vitesse_G(tr/min)");
 }
 
 void loop() {
   int Value_JX = analogRead(A2);
   unsigned long currentMicros = micros();
+  unsigned long t = millis() - debutEchelon; // Temps écoulé depuis le début du test
 
   switch (cas) {
-    case 0:
+    case 0: // Attente du signal de départ (Joystick)
       if (Value_JX >= 700) {
-        previousMicros = micros(); // Reset du timer avant de commencer
+        debutEchelon = millis(); 
         cas = 1;
       }
       break;
 
     case 1:
-      MoteurGD(200, 400);
+      if (t < 2000) {
+        MoteurGD(400, 400); // 0V à 5V
+      } 
+      else if (t < 4000) {
+        MoteurGD(67, 67);   // echelon à 5V
+      } 
+      else if (t < 6000) {
+        MoteurGD(0, 0);     // echelon à 6V
+      } 
+      else {
+        StopMoteurGD;       // arret
+        cas = 2; 
+      }
 
       if (currentMicros - previousMicros >= TE_US) {
-
         long newRight = knobRight.read();
         long deltaP_R = newRight - oldRight;
 
+        // Calcul vitesse en tr/min
         float tr_min_R = (deltaP_R / N_IMP) / (TE_US / 60000000.0);
-
+        
+        // Affichage de la vitesse par rapport au temps
+        Serial.print(t);
+        Serial.print(",");
         Serial.println(tr_min_R);
 
         oldRight = newRight;
         previousMicros = currentMicros;
       }
+      break;
+      
+    case 2:
       break;
   }
 }
